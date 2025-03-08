0830089a228ade8ab8507cdfefb0739febdcff5b
Benjamin Cuison <benjamincuison@email.grcc.edu>
2025-03-08 15:51:13 -0500

    Fixed connStrings to point to new database and added a web layout page and data access folder

---
+++ CIS229C-II-Project/DataAccessLayer/DataAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CIS229C_II_Project.DataAccessLayer
+{
+    public class DataAccess
+    {
+        public Boolean Example ()
+        {
+            return false;
+        }
+    }
+}
