7c5af8c5076843c8e0a0702370fe4de6502192a2
Benjamin Cuison <benjamincuison@email.grcc.edu>
2025-03-09 20:18:13 -0400

    Added JobDTO class to store data from the db. Created a GetJobDTOList method in DataAccess to bring data from the db to front end. Also added and started Dashboard View and Controller.

---
+++ CIS229C-II-Project/Controllers/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CIS229C_II_Project.DataAccessLayer;
+
+namespace CIS229C_II_Project.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Dashboard()
+        {
+            DataAccess data = new DataAccess();
+            List<Models.JobDTO> userList = data.GetJobDTOList();
+            return View("Dashboard");
+        }
+    }
+}
--- CIS229C-II-Project/DataAccessLayer/DataAccess.cs
+++ CIS229C-II-Project/DataAccessLayer/DataAccess.cs
@@ -1,15 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Services.Description;
+using CIS229C_II_Project.Models;
 
 namespace CIS229C_II_Project.DataAccessLayer
 {
     public class DataAccess
     {
-        public Boolean Example ()
+        public List<JobDTO> GetJobDTOList ()
         {
-            return false;
+            List<JobDTO> jobDTOList = new List<JobDTO>();
+            String connString = ConfigurationManager.ConnectionStrings["benConnString"].ToString();
+            //String connString = ConfigurationManager.ConnectionStrings["dustinConnString"].ToString();
+            SqlConnection sqlConnection = new SqlConnection(connString);
+            try
+            {
+                sqlConnection.Open();
+                string query = "";
+                SqlCommand cmd = new SqlCommand(query, sqlConnection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandTimeout = 0;
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        JobDTO job = new JobDTO();
+                        job.JobID = Convert.ToInt32(reader["JobID"].ToString());
+                        job.CustomerID = Convert.ToInt32(reader["CustomerID"].ToString());
+                        job.CustomerFirstName = reader["CustomerFirstName"].ToString();
+                        job.CustomerLastName = reader["CustomerLastName"].ToString();
+
+                        if (reader["CustomerPhone"] == null)
+                        {
+                            job.CustomerPhone = "";
+                        }
+                        else
+                        {
+                            job.CustomerPhone = reader["CustomerPhone"].ToString();
+                        }
+
+                        if (reader["CustomerEmail"] == null)
+                        {
+                            job.CustomerEmail = "";
+                        }
+                        else
+                        {
+                            job.CustomerEmail = reader["CustomerEmail"].ToString();
+                        }
+
+                        
+                        job.TechnicianName = reader["TechnicianName"].ToString();
+                        job.JobCreated = Convert.ToDateTime(reader["JobCreated"].ToString());
+
+                        if (reader["JobCompleted"] == null)
+                        {
+                            job.JobCompleted= null;
+                        }
+                        else
+                        {
+                            job.JobCompleted = Convert.ToDateTime(reader["JobCreated"].ToString());
+                        }
+
+                        job.ServiceID = Convert.ToInt32(reader["ServiceID"].ToString());
+                        job.ServiceName = reader["ServiceName"].ToString();
+                        job.ServiceDescription = reader["ServiceDescription"].ToString();
+                        job.ServiceCost = Convert.ToDouble(reader["Cost"].ToString());
+
+                        jobDTOList.Add(job);
+
+                    }
+                }
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                sqlConnection.Close();
+                return null;
+            }
+            
+            return jobDTOList;
+                
         }
     }
 }
