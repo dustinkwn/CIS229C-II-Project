67c3c928beb55b5338d871e07485b2ff4c619bd4
Benjamin Cuison <benjamincuison@email.grcc.edu>
2025-03-23 19:05:28 -0400

    Created halfway finished job controller, data access, and views (not finished!).

---
+++ CIS229C-II-Project/Controllers/JobController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CIS229C_II_Project.DataAccessLayer;
+
+namespace CIS229C_II_Project.Controllers
+{
+    public class JobController : Controller
+    {
+        // GET: Job
+        [HttpGet]
+        public ActionResult CreateJob()
+        {
+            List<Models.Customer> customerList = new List<Models.Customer>();
+            CustomerDataAccess customerData = new CustomerDataAccess();
+            customerList = customerData.GetCustomerList();
+            return View(customerList);
+        }
+        [HttpGet]
+        public ActionResult EditJob()
+        {
+            List<Models.JobDTO> jobDTOList = new List<Models.JobDTO>();
+            DataAccess jobDTOData = new DataAccess();
+            jobDTOList = jobDTOData.GetJobDTOList();
+            return View(jobDTOList);
+        }
+        [HttpGet]
+        public ActionResult DeleteJob()
+        {
+            List<Models.Job> jobList = new List<Models.Job>();
+            JobDataAccess jobData = new JobDataAccess();
+            jobList = jobData.GetJobList();
+            return View(jobList);
+        }
+
+    }
+}
---
+++ CIS229C-II-Project/Models/Job.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CIS229C_II_Project.Models
+{
+    public class Job
+    {
+        public Job() { }
+        public int ID { get; set; }
+        public string Technician { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Finished { get; set; }
+        public int CustomerID { get; set; }
+    }
+}
