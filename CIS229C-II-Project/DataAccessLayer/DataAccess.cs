7fd22345b978ce20f7814845e4b5b531f41dcd26
Benjamin Cuison <benjamincuison@email.grcc.edu>
2025-04-03 15:42:24 -0400

    Halfway finished / started work on the job class, controller, and views.

--- CIS229C-II-Project/DataAccessLayer/DataAccess.cs
+++ CIS229C-II-Project/DataAccessLayer/DataAccess.cs
@@ -12,79 +12,19 @@ namespace CIS229C_II_Project.DataAccessLayer
 {
     public class DataAccess
     {
-        public List<JobDTO> GetJobDTOList ()
+        public JobDTO GetJobDTO ()
         {
-            List<JobDTO> jobDTOList = new List<JobDTO>();
-            String connString = ConfigurationManager.ConnectionStrings["benConnString"].ToString();
-            //String connString = ConfigurationManager.ConnectionStrings["dustinConnString"].ToString();
-            SqlConnection sqlConnection = new SqlConnection(connString);
-            try
-            {
-                sqlConnection.Open();
-                string query = "";
-                SqlCommand cmd = new SqlCommand(query, sqlConnection);
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.CommandTimeout = 0;
-                using (SqlDataReader reader = cmd.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        JobDTO job = new JobDTO();
-                        job.JobID = Convert.ToInt32(reader["JobID"].ToString());
-                        job.CustomerID = Convert.ToInt32(reader["CustomerID"].ToString());
-                        job.CustomerFirstName = reader["CustomerFirstName"].ToString();
-                        job.CustomerLastName = reader["CustomerLastName"].ToString();
+            CustomerDataAccess customerData = new CustomerDataAccess ();
+            ServiceDataAccess serviceData = new ServiceDataAccess ();
+            JobDataAccess jobDataAccess = new JobDataAccess ();
 
-                        if (reader["CustomerPhone"] == null)
-                        {
-                            job.CustomerPhone = "";
-                        }
-                        else
-                        {
-                            job.CustomerPhone = reader["CustomerPhone"].ToString();
-                        }
+            JobDTO dtoData = new JobDTO ();
+            dtoData.CustomerList = customerData.GetCustomerList ();
+            //dtoData.ServiceList = serviceData.GetServiceList();
+            dtoData.JobList = jobDataAccess.GetJobList ();
+            return dtoData;
 
-                        if (reader["CustomerEmail"] == null)
-                        {
-                            job.CustomerEmail = "";
-                        }
-                        else
-                        {
-                            job.CustomerEmail = reader["CustomerEmail"].ToString();
-                        }
-
-                        
-                        job.TechnicianName = reader["TechnicianName"].ToString();
-                        job.JobCreated = Convert.ToDateTime(reader["JobCreated"].ToString());
-
-                        if (reader["JobCompleted"] == null)
-                        {
-                            job.JobCompleted= null;
-                        }
-                        else
-                        {
-                            job.JobCompleted = Convert.ToDateTime(reader["JobCreated"].ToString());
-                        }
-
-                        job.ServiceID = Convert.ToInt32(reader["ServiceID"].ToString());
-                        job.ServiceName = reader["ServiceName"].ToString();
-                        job.ServiceDescription = reader["ServiceDescription"].ToString();
-                        job.ServiceCost = Convert.ToDouble(reader["Cost"].ToString());
-
-                        jobDTOList.Add(job);
-
-                    }
-                }
-                sqlConnection.Close();
-            }
-            catch (Exception ex)
-            {
-                sqlConnection.Close();
-                return null;
-            }
-            
-            return jobDTOList;
-                
         }
+       
     }
 }
--- CIS229C-II-Project/Models/JobDTO.cs
+++ CIS229C-II-Project/Models/JobDTO.cs
@@ -7,39 +7,32 @@ namespace CIS229C_II_Project.Models
 {
     public class JobDTO
     {
-        // This Class is used to transfer data from a query to local objects
-        public JobDTO() { }
-        public JobDTO(int jobID,int customerID, string customerFirstName, string customerLastName, string customerPhone, string customerEmail, 
-            string technicianName, DateTime jobCreated, DateTime jobCompleted,int serviceID, string serviceName, string serviceDescription,
-            double serviceCost) 
+        // This Class is used to transfer data from a query to local objects Holds all the data.
+        public JobDTO()
         {
-            JobID = jobID;
-            CustomerID = customerID;
-            CustomerFirstName = customerFirstName;
-            CustomerLastName = customerLastName;
-            CustomerPhone = customerPhone;
-            CustomerEmail = customerEmail;
-            TechnicianName = technicianName;
-            JobCreated = jobCreated;
-            JobCompleted = jobCompleted;
-            ServiceID = serviceID;
-            ServiceName = serviceName;
-            ServiceDescription = serviceDescription;
-            ServiceCost = serviceCost;
+            CustomerList = new List<Customer>(); 
+            ServiceList = new List<Service>();
+            JobList = new List<Job>();
         }
-        public int JobID { get; set; }
-        public int CustomerID {  get; set; }
-        public string CustomerFirstName { get; set; }
-        public string CustomerLastName { get; set; }
-        public string CustomerPhone { get; set; }
-        public string CustomerEmail { get; set; }
-        public string TechnicianName { get; set; }
-        public DateTime JobCreated { get; set; }
-        public DateTime? JobCompleted { get; set; }
-        public int ServiceID { get; set; }
-        public string ServiceName { get; set; }
-        public string ServiceDescription { get; set; }
-        public double ServiceCost { get; set; }
+
+        //public int JobID { get; set; }
+        //public int CustomerID {  get; set; }
+        //public string CustomerFirstName { get; set; }
+        //public string CustomerLastName { get; set; }
+        //public string CustomerPhone { get; set; }
+        //public string CustomerEmail { get; set; }
+        //public string TechnicianName { get; set; }
+        //public DateTime JobCreated { get; set; }
+        //public DateTime? JobFinished { get; set; }
+        //public int ServiceID { get; set; }
+        //public string ServiceName { get; set; }
+        //public string ServiceDescription { get; set; }
+        //public double ServiceCost { get; set; }
+        public List<Customer> CustomerList { get; set; }
+        public List<Service> ServiceList { get; set; }
+        public List<Job> JobList { get; set; }
+
+        
     }
     
 }
