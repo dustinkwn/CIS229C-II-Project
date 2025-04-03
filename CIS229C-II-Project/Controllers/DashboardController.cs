0a3912c89620539f5314aeb362919bfc9b5291d8
Benjamin Cuison <benjamincuison@email.grcc.edu>
2025-04-03 16:15:48 -0400

    Commented Error

--- CIS229C-II-Project/Controllers/DashboardController.cs
+++ CIS229C-II-Project/Controllers/DashboardController.cs
@@ -13,7 +13,7 @@ namespace CIS229C_II_Project.Controllers
         public ActionResult Dashboard()
         {
             DataAccess data = new DataAccess();
-            List<Models.JobDTO> userList = data.GetJobDTOList();
+            //List<Models.JobDTO> userList = data.GetJobDTOList();
             return View("Dashboard");
         }
     }
